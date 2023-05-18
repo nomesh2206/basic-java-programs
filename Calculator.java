d08e3e609604640d760bb0bc323e4057550a031e
sanketda <133940723+sanketda@users.noreply.github.com>
2023-05-18 12:17:23 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Check_prime 
+
+{
+  public static void main(String []a)
+ {
+   int n=11, m;
+   m= n/2;
+   if ( m%2==0)
+
+ {  System.out.println("non prime"+n);
+}
+
+ if( m%2==1)
+{ System.ou.println("prime no " +n);
+}
+    
+}
+}
