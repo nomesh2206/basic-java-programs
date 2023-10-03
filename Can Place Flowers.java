a507fbe0d6a58babecd5b1d3df3b2446b445e726
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-10-03 23:51:12 +0530

    Create Can Place Flowers.java

---
+++ Can Place Flowers.java
@@ -0,0 +1,14 @@
+lass Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count=0;
+      for (int i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i]==0 && (((i == 0) || (flowerbed[i - 1] == 0)) && ((i == flowerbed.length - 1) || (flowerbed[i + 1] == 0)))) {
+					flowerbed[i] = 1;
+					count++;
+						if (count >= n)
+							return count>=n;
+        }
+      }
+        return count>=n;
+    }
+}
