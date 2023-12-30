7d892871cc2c2a1b50a0ff8f07577070a057e741
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2023-12-31 00:03:44 +0530

    Update Fruits_into_basket.java

--- Fruits_into/Fruits_into_basket.java
+++ Fruits_into/Fruits_into_basket.java
@@ -1,20 +1,19 @@
 class Solution {
     public int totalFruit(int[] tree) {
-        HashMap<Integer, Integer> count = new HashMap<Integer, Integer>();
-        int firstIdx = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int firstindx = 0;
         int totalMax = 0;
 
         for (int i = 0; i < tree.length; i++) {
-            count.put(tree[i], count.getOrDefault(tree[i], 0) + 1);
-            while (count.size() > 2) {
-                count.put(tree[firstIdx], count.get(tree[firstIdx]) - 1);
-                if (count.get(tree[firstIdx]) == 0) {
-                    count.remove(tree[firstIdx]);
+            map.put(tree[i], map.getOrDefault(tree[i], 0) + 1);
+            while (map.size() > 2) {
+                map.put(tree[firstindx], map.get(tree[firstindx]) - 1);
+                if (map.get(tree[firstindx]) == 0) {
+                    map.remove(tree[firstindx]);
                 }
-                firstIdx++;
+                firstindx++;
             }
-            totalMax = Math.max(totalMax, i - firstIdx + 1);
+            totalMax = Math.max(totalMax, i - firstind + 1);
         }
         return totalMax;
     }
-}
