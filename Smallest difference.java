139ab420987f9307fa87d5acadebcf43a3b8284c
Nomesh Verma <43084989+nomesh2206@users.noreply.github.com>
2024-01-02 00:02:18 +0530

    Update Smallest difference.java

--- Smallest difference.java
+++ Smallest difference.java
@@ -1,1 +1,26 @@
+public class Solution {
+    /*
+     * @param A, B: Two integer arrays.
+     * @return: Their smallest difference.
+     */
+    public int smallestDifference(int[] A, int[] B) {
+        if (A == null || B == null || A.length == 0 || B.length == 0) {
+            return 0;
+        }
 
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int i = 0, j = 0;
+        int minDiff = Integer.MAX_VALUE;
+        while (i < A.length && j < B.length) {
+            minDiff = Math.min(Math.abs(A[i] - B[j]), minDiff);
+            if (A[i] < B[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return minDiff;
+    }
+}
